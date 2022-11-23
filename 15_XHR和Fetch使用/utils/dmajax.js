837b194cdff0978d00d093affd22545dd130067d
CoderDuMin <1325964835@qq.com>
2022-11-23 20:00:16 +0800

    xhr封装、fetch使用、文件上传

---
+++ 15_XHR和Fetch使用/utils/dmajax.js
@@ -0,0 +1,40 @@
+function dmajax({
+  url,
+  method = "get",
+  data = {},
+  headers = {}, // token
+  success,
+  failure
+} = {}) {
+  // 1.创建对象
+  const xhr = new XMLHttpRequest()
+
+  // 2.监听数据
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      success && success(xhr.response)
+    } else {
+      failure && failure({ status: xhr.status, message: xhr.statusText })
+    }
+  }
+
+  // 3.设置类型
+  xhr.responseType = "json"
+
+  // 4.open方法
+  if (method.toUpperCase() === "GET") {
+    const queryStrings = []
+    for (const key in data) {
+      queryStrings.push(`${key}=${data[key]}`)
+    }
+    url = url + "?" + queryStrings.join("&")
+    xhr.open(method, url)
+    xhr.send()
+  } else {
+    xhr.open(method, url)
+    xhr.setRequestHeader("Content-type", "application/json")
+    xhr.send(JSON.stringify(data))
+  }
+
+  return xhr
+}
---
+++ 15_XHR和Fetch使用/utils/dmajax_promise.js
@@ -0,0 +1,41 @@
+function dmajax({
+  url,
+  method = "get",
+  data = {},
+  headers = {}, // token
+  success,
+  failure
+} = {}) {
+  // 1.创建对象
+  const xhr = new XMLHttpRequest()
+  return new Promise((resolve,reject) => {
+      
+    // 2.监听数据
+    xhr.onload = function() {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response)
+      } else {
+        reject({ status: xhr.status, message: xhr.statusText })
+      }
+    }
+
+    // 3.设置类型
+    xhr.responseType = "json"
+
+    // 4.open方法
+    if (method.toUpperCase() === "GET") {
+      const queryStrings = []
+      for (const key in data) {
+        queryStrings.push(`${key}=${data[key]}`)
+      }
+      url = url + "?" + queryStrings.join("&")
+      xhr.open(method, url)
+      xhr.send()
+    } else {
+      xhr.open(method, url)
+      xhr.setRequestHeader("Content-type", "application/json")
+      xhr.send(JSON.stringify(data))
+    }
+  })
+
+}
